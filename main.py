25ecba6886435ea88a1ade91d803671a2a9b1420
RobsenGelana <robsengelana21@gmail.com>
2022-09-23 14:39:56 +0300

    adding comments

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ from turtle import Screen, Turtle
 from paddle import Paddle
 
 screen = Screen()
+turtle = Turtle()
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
 screen.title('Pong')
 screen.tracer(0)
 
+#Creating object for the paddle
 l_paddle = Paddle((350, 0))
 r_paddle = Paddle((-350, 0))
-turtle = Turtle()
+
+#Listening for keyboard keys
 screen.listen()
 screen.onkey(l_paddle.move_up, 'Up')
 screen.onkey(l_paddle.move_down, 'Down') 
 screen.onkey(r_paddle.move_up, 'w')
 screen.onkey(r_paddle.move_down, 's') 
 
+#creating a while loop to update the screen
 game_on = True
 while game_on:
     screen.update()
