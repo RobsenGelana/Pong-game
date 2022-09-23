de23e82fbc4fbf829c1196a7525a58ddfdc2842e
RobsenGelana <robsengelana21@gmail.com>
2022-09-23 15:17:04 +0300

    giving the class a parameter

--- ball.py
+++ ball.py
@@ -1,7 +1,9 @@
-from turtle import Turtle
+from turtle import Turtle, position
 
 class Ball(Turtle):
-    def __init__(self) -> None:
+    def __init__(self, position):
         super().__init__()
         self.shape('circle')
+        self.penup()
         self.color('white')
+        self.goto(position)
