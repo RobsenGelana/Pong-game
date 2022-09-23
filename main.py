1d89734d02b5a299dc541857b04bc016d4172fdf
RobsenGelana <robsengelana21@gmail.com>
2022-09-23 14:25:37 +0300

    removing the animation from the screen and updating the screen

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
 screen.title('Pong')
+screen.tracer(0)
 
 paddle = Paddle()
 turtle = Turtle()
@@ -12,6 +13,9 @@ screen.listen()
 screen.onkey(paddle.move_up, 'Up')
 screen.onkey(paddle.move_down, 'Down') 
 
+game_on = True
+while game_on:
+    screen.update()
 
 
 
