9c6a576021b02d83e1f888d49f3bfc9d5cc11e61
RobsenGelana <robsengelana21@gmail.com>
2022-09-23 16:08:30 +0300

    checking if the ball make a collision with the paddels

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ screen.title('Pong')
 screen.tracer(0)
 
 #Creating object for the paddle
-l_paddle = Paddle((350, 0))
-r_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 #creating a ball object
 ball = Ball()
 #Listening for keyboard keys
 screen.listen()
-screen.onkey(l_paddle.move_up, 'Up')
-screen.onkey(l_paddle.move_down, 'Down') 
-screen.onkey(r_paddle.move_up, 'w')
-screen.onkey(r_paddle.move_down, 's') 
+screen.onkey(r_paddle.move_up, 'Up')
+screen.onkey(r_paddle.move_down, 'Down') 
+screen.onkey(l_paddle.move_up, 'w')
+screen.onkey(l_paddle.move_down, 's') 
 
 
 #creating a while loop to update the screen
@@ -32,8 +32,12 @@ while game_on:
     screen.update()
     ball.move()
 
-    if ball.ycor() > 300 or ball.ycor() < -300:
-        ball.bounce() 
+    #Detecting ball collision with the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y() 
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() > -320:
+        ball.bounce_x()
 
 
 screen.exitonclick()
