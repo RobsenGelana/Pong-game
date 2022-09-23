5259d4a12a4bf08492ed4573bc162eded4004da2
RobsenGelana <robsengelana21@gmail.com>
2022-09-23 14:34:36 +0300

    creating left and right object from the paddle class to make the paddle move up and down

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ screen.bgcolor('black')
 screen.title('Pong')
 screen.tracer(0)
 
-paddle = Paddle()
+l_paddle = Paddle((350, 0))
+r_paddle = Paddle((-350, 0))
 turtle = Turtle()
 screen.listen()
-screen.onkey(paddle.move_up, 'Up')
-screen.onkey(paddle.move_down, 'Down') 
+screen.onkey(l_paddle.move_up, 'Up')
+screen.onkey(l_paddle.move_down, 'Down') 
+screen.onkey(r_paddle.move_up, 'w')
+screen.onkey(r_paddle.move_down, 's') 
 
 game_on = True
 while game_on:
