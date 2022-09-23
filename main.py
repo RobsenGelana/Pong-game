7c1908851f423194efb848a56f1f017724865e72
RobsenGelana <robsengelana21@gmail.com>
2022-09-23 14:15:01 +0300

    my commit

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from turtle import Screen
-
+from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
 screen.title('Pong')
 
-
-
+paddle = Paddle()
+turtle = Turtle()
+screen.listen()
+screen.onkey(paddle.move_up, 'Up')
+screen.onkey(paddle.move_down, 'Down') 
 
 
 
